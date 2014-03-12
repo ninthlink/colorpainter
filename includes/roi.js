9965383f8912abb329c68af1f395afce1e693bd0
alex chousmith <alex@ninthlink.com>
2014-03-12 06:27:38 +0000

    some optimizations and removing experimental touch icon

--- includes/roi.js
+++ includes/roi.js
@@ -79,7 +79,7 @@ Jetrix KX5
 */
 var defaultPrinter = 0; // index to choose
 var roiURL = 'http://ninthlink.me/seiko/calc/';
-var roiDebug = false; // true = console.log debug msgs ?
+//var roiDebug = false; // true = console.log debug msgs ?
 var roiOpts = {
 	aSign: '$'
 };
@@ -87,37 +87,31 @@ var roiUpdater;
 var roiSave = '';
 
 function roi_update() {
-	roi_dbg('## ROI Update :: printer change');
+	//roi_dbg('## ROI Update :: printer change');
 	
 	var C14 = $('#printer').val();
-	roi_dbg('printer #'+ C14);
-	roi_dbg(printers[C14]);
+	//roi_dbg('printer #'+ C14);
+	//roi_dbg(printers[C14]);
 	
 	if ( C14 > 0 ) {
-		// undisable things?
+		// enable things?
 		var sibs = $('#printer').parents('fieldset').siblings().removeClass('disabled');
-		/*
-		sibs.find('.ui-input-text input').prop('disabled',false).textinput('enable').textinput('refresh');
-		sibs.find('.ui-select select').prop('disabled',false).selectmenu('enable').selectmenu('refresh');
-		sibs.find('.ui-radio input').prop('disabled',false).checkboxradio('enable').checkboxradio('refresh');
-		*/
-		
 		sibs.find('.ui-input-text input').textinput('enable');
 		sibs.find('.ui-select select').selectmenu('enable');
 		sibs.find('.ui-radio input').checkboxradio('enable');
 		
 		var C15 = printers[C14]['msrp'];
-		roi_dbg('psellprice = '+ C15);
+		//roi_dbg('psellprice = '+ C15);
 		$('#psellprice').val(C15).autoNumeric('update');
 		$('#spsellprice').val(C15).autoNumeric('update');
 		
 		var C16 = printers[C14]['speed'];
-		roi_dbg('pspeed = '+ C16);
+		//roi_dbg('pspeed = '+ C16);
 		$('#pspeed').val(C16).autoNumeric('update');
 		$('#spspeed').val(C16).autoNumeric('update');
 		
 		var C17 = printers[C14]['ink'];
-		roi_dbg('pinkpersqft = '+ C17);
+		//roi_dbg('pinkpersqft = '+ C17);
 		$('#pinkpersqft').val(C17).autoNumeric('update');
 		$('#spinkpersqft').val(C17).autoNumeric('update');
 		
@@ -138,7 +132,7 @@ function roi_update() {
 }
 
 function roi_recalc(e) {
-	roi_dbg('## ROI Update :: recalc');
+	//roi_dbg('## ROI Update :: recalc');
 	
 	var C4 = $('#price').autoNumeric('get');
 	var C5 = $('#speed').autoNumeric('get');
@@ -150,19 +144,19 @@ function roi_recalc(e) {
 	$('#srollsperwk').val(C7);
 	
 	var C8 = 600 * C7;
-	roi_dbg('C8 "sqftperwk" = '+ C8);
+	//roi_dbg('C8 "sqftperwk" = '+ C8);
 	$('#sqftperwk').val(C8).autoNumeric('update');
 	$('#ssqftperwk').val(C8).autoNumeric('update');
 	
 	var C9 = roiRound((C6*C8)*(52/12));
-	roi_dbg('C9 "inkpermonth" = '+ C9);
+	//roi_dbg('C9 "inkpermonth" = '+ C9);
 	$('#inkpermonth').val(C9).autoNumeric('update');
 	$('#sinkpermonth').val(C9).autoNumeric('update');
 	
 	var C10 = $('#amortper-5').prop('checked') ? 5 : 3;
 	
 	var C11 = roiRound(C4/(C10*12));
-	roi_dbg('C11 "monthlyamort" = '+ C11);
+	//roi_dbg('C11 "monthlyamort" = '+ C11);
 	$('#monthlyamort').val(C11).autoNumeric('update');
 	$('#smonthlyamort').val(C11).autoNumeric('update');
 	
@@ -172,47 +166,47 @@ function roi_recalc(e) {
 	var C17 = $('#pinkpersqft').autoNumeric('get');
 	
 	var C19 = C8;
-	roi_dbg('C17 "psqftperwk" = '+ C19);
+	//roi_dbg('C17 "psqftperwk" = '+ C19);
 	$('#psqftperwk').val(C19);
 	$('#spsqftperwk').val(C19);
 	
 	var C20 = roiRound((C17*C19)*(52/12));
-	roi_dbg('C20 "pinkpermonth: = '+ C20);
+	//roi_dbg('C20 "pinkpermonth: = '+ C20);
 	$('#pinkpermonth').val(C20).autoNumeric('update');
 	$('#spinkpermonth').val(C20).autoNumeric('update');
 	
 	var C21 = C10;
-	roi_dbg('C21 "pamortper" = '+ C21);
+	//roi_dbg('C21 "pamortper" = '+ C21);
 	$('#pamortper, #samortper, #spamortper').val(C21);
 	
 	var C22 = roiRound(C15/(C21*12));
-	roi_dbg('C22 "pmonthlyamort" = '+ C22);
+	//roi_dbg('C22 "pmonthlyamort" = '+ C22);
 	$('#pmonthlyamort').val(C22).autoNumeric('update');
 	$('#spmonthlyamort').val(C22).autoNumeric('update');
 	
 	var C25 = C11+C9;
-	roi_dbg('C25 "cmonthlytotal" = '+ C25);
+	//roi_dbg('C25 "cmonthlytotal" = '+ C25);
 	$('#cmonthlytotal').val(C25).autoNumeric('update');
 	
 	var C26 = C22+C20;
-	roi_dbg('C26 "monthlytotal" = '+ C26);
+	//roi_dbg('C26 "monthlytotal" = '+ C26);
 	$('#monthlytotal').val(C26).autoNumeric('update');
 	
 	var C27 = roiRound((C9+C11)-(C20+C22));
-	roi_dbg('C27 "costpermonth" = '+ C27);
+	//roi_dbg('C27 "costpermonth" = '+ C27);
 	var cpmzs = '' + ( C27 >= 1000 ? 0 : 2 );
 	$('#costpermonth').val(C27).autoNumeric('update', { mDec: cpmzs });
 	// & check for "Savings" vs "Loss"
 	$('#costpermonth').parent().siblings('label').find('.save').html( C27 >= 0 ? 'Savings' : 'Loss');
 	
 	var C28 = C27*(C21*12);
-	roi_dbg('C28 "additional" = '+ C28);
+	//roi_dbg('C28 "additional" = '+ C28);
 	$('#additional').val(C28).autoNumeric('update');
 	// & check for "Savings" vs "Loss"
 	$('#additional').parent().siblings('label').find('.save').html( C28 >= 0 ? 'Profit' : 'Loss');
 	
 	var C29 = roiRound(((C8/C5)-(C19/C16))*52/12);
-	roi_dbg('C29 "esthrs" = '+ C29);
+	//roi_dbg('C29 "esthrs" = '+ C29);
 	$('#esthrs').val(C29).autoNumeric('update');
 	// & check for "Savings" vs "Loss"
 	$('#esthrs').parent().siblings('label').find('.save').html( C29 >= 0 ? 'Savings' : 'Loss');
@@ -235,21 +229,21 @@ function roi_save(C10, C14, C4, C5, C600, C7) {
 	C7 = '' + C7;
 	// C6 = comp ink per sqft cost
 	roiSave = C14a + C7 + C4a.length + C4a + C5a.length + C5a + C6a;
-	roi_dbg('C10 ' + C10 + ' C14 ' + C14 + ' : C14a ' + C14a + ' C7 ' + C7 + ' C4L ' + C4a.length + ' C4a ' + C4a + ' C5L ' + C5a.length + ' C5a ' + C5a + ' C6a ' + C6a + ' :: ' + roiSave);
+	//roi_dbg('C10 ' + C10 + ' C14 ' + C14 + ' : C14a ' + C14a + ' C7 ' + C7 + ' C4L ' + C4a.length + ' C4a ' + C4a + ' C5L ' + C5a.length + ' C5a ' + C5a + ' C6a ' + C6a + ' :: ' + roiSave);
 	$('#saver').val( roiURL + '?s=' + roiSave.toUpperCase());
 }
 
 function roi_load(save) {
 	// reverse roi_save, so
 	save = save.toLowerCase();
-	roi_dbg('LOADING "'+ save + ' ...');
+	//roi_dbg('LOADING "'+ save + ' ...');
 	var C14 = roiBase36toInt(save[0]);
 	var C10 = 5;
 	if ( C14 > 18 ) {
 		C14 = C14 - 18;
 		C10 = 3;
 	}
-	roi_dbg('... AMORT PER = ' + C10 + ', printer selected #' + C14 + ' ...');
+	//roi_dbg('... AMORT PER = ' + C10 + ', printer selected #' + C14 + ' ...');
 	// to change the printer select, we are loading this before the printer dropdown list populates, so just need to..
 	defaultPrinter = ( C14 < printers.length ? C14 : 0 );
 	// update radios
@@ -260,26 +254,26 @@ function roi_load(save) {
 	try{
 		$('#rollsperwk').selectmenu('refresh');
 	} catch(e) {
-		roi_dbg('xxxx selectmenu refresh fail xxxx');
+		//roi_dbg('xxxx selectmenu refresh fail xxxx');
 	}
 	var d = parseInt(save[2]);
 	var C4a = save.substr(3,d);
 	var C4 = roiBase36toInt(C4a);
-	roi_dbg('C4a = ' + C4a + ' so = ' + C4);
+	//roi_dbg('C4a = ' + C4a + ' so = ' + C4);
 	$('#price').val(C4).autoNumeric('update');
 	
 	save = save.substr(3+d);
-	roi_dbg('rest of save becomes : ' + save);
+	//roi_dbg('rest of save becomes : ' + save);
 	d = parseInt(save[0]);
 	var C5a = save.substr(1,d);
 	var C5 = roiBase36toInt(C5a);
-	roi_dbg('C5a = ' + C5a + ' so = ' + C5);
+	//roi_dbg('C5a = ' + C5a + ' so = ' + C5);
 	$('#speed').val(C5).autoNumeric('update');
 	
 	save = save.substr(1+d);
-	roi_dbg('rest of save becomes : ' + save);
+	//roi_dbg('rest of save becomes : ' + save);
 	var C6 = roiBase36toInt(save) / 100;
-	roi_dbg('C6 = ' + C6);
+	//roi_dbg('C6 = ' + C6);
 	$('#inkpersqft').val(C6).autoNumeric('update');
 }
 
@@ -287,15 +281,15 @@ function roi_throttle(e) {
 	clearTimeout(roiUpdater);
 	roiUpdater = setTimeout(roi_recalc, 100);
 }
-
+/*
 function roi_dbg(m) {
 	if ( roiDebug ) {
 		console.log(m);
 	}
 }
-
+*/
 function roi_amortper(n) {
-	roi_dbg('roi_amortper : '+ n);
+	//roi_dbg('roi_amortper : '+ n);
 	if ( n != 3 ) {
 		n = 5;
 	}
@@ -319,7 +313,7 @@ jQuery(document).one('pagechange', function() {
 			roi_recalc();
 		},
 		'roiCheck': function() {
-			roi_dbg('vv roiCheck : '+ $(this).attr('id'));
+			//roi_dbg('vv roiCheck : '+ $(this).attr('id'));
 			$(this).prop('checked',true).trigger('roiChecked');
 		},
 		'roiChecked': function() {
@@ -327,7 +321,7 @@ jQuery(document).one('pagechange', function() {
 			try {
 				$('input[type="radio"]').checkboxradio('refresh');
 			} catch(e) {
-				roi_dbg('xxxx checkboxradio refresh fail xxxx');
+				//roi_dbg('xxxx checkboxradio refresh fail xxxx');
 			}
 		}
 	});
@@ -339,7 +333,7 @@ jQuery(document).one('pagechange', function() {
 		roi_load(sget);
 	} else {
 		roi_amortper(5);
-		roi_dbg('NO SAVE FOUND present');
+		//roi_dbg('NO SAVE FOUND present');
 		// just default amortper-5 on
 	}
 	
@@ -359,7 +353,7 @@ jQuery(document).one('pagechange', function() {
 			try{
 				$(this).selectmenu('refresh');
 			} catch(e) {
-				roi_dbg('xxxx selectmenu refresh fail xxxx');
+				//roi_dbg('xxxx selectmenu refresh fail xxxx');
 			}
 			roi_update();
 		}
@@ -378,11 +372,13 @@ jQuery(document).one('pagechange', function() {
 		$(this).children('.ui-field-contain:even').addClass('even');
 	});
 	
-	$(document).on("click","#saver",function(a){a.preventDefault();selectText($(this));});
+	$("#saver").focus(function(e){e.preventDefault();selectText($(this));}).mouseup(function(e) { e.preventDefault(); }).on('tap', function() {
+		selectText($(this));
+	}).on('vmouseup', function(e) { e.preventDefault(); });
 	
 	// bind for subsequent updates
 	$(document).bind('pagechange', function() {
-		roi_dbg('PPPPPPPP PAGELOAD PPPPPPP');
+		//roi_dbg('PPPPPPPP PAGELOAD PPPPPPP');
 		roi_update();
 	});
 });
@@ -404,5 +400,5 @@ function roiBase36toInt(alph) {
 	return i;
 }
 
-function selectText(t){console.log('**selecttext'); var a=t[0],b,c;"INPUT"===a.tagName?a.select():document.body.createTextRange?(b=document.body.createTextRange(),b.moveToElementText(a),b.select()):window.getSelection&&(c=window.getSelection(),b=document.createRange(),
+function selectText(t){var a=t[0],b,c;"INPUT"===a.tagName?a.select():document.body.createTextRange?(b=document.body.createTextRange(),b.moveToElementText(a),b.select()):window.getSelection&&(c=window.getSelection(),b=document.createRange(),
 b.selectNodeContents(a),c.removeAllRanges(),c.addRange(b))}
