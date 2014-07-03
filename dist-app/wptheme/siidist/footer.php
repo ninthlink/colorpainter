fc97984e3eab34ebed3c4e26441d43f52c037ec7
alex chousmith <alex@ninthlink.com>
2014-07-03 06:03:22 +0000

    hoempage & header / footer div cleanup

--- dist-app/wptheme/siidist/footer.php
+++ dist-app/wptheme/siidist/footer.php
@@ -9,20 +9,20 @@
 ?>
 
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-footer">
 				<div class="site-info">
 					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'siidist' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'siidist' ), 'WordPress' ); ?></a>
 					<span class="sep"> | </span>
 					<?php printf( __( 'Theme: %1$s by %2$s.', 'siidist' ), 'siidist', '<a href="http://www.ninthlink.com" rel="designer">Ninthlink, Inc.</a>' ); ?>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
+				</div>
+			</div>
 	
 	
 
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	</div><!-- #content -->
-</div><!-- #page -->
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php wp_footer(); ?>
 
--- dist-app/wptheme/siidist/header.php
+++ dist-app/wptheme/siidist/header.php
@@ -19,24 +19,16 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-
-	<div id="content" class="site-content">
-
-		<header id="masthead" class="site-header" role="banner">
-			<div class="site-branding">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</div>
-
-			<nav id="site-navigation" class="main-navigation" role="navigation">
+<div class="rap">
+	<div class="site-content">
+		<div class="site-header">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a>
+			<div class="main-navigation" role="navigation">
 				<button class="menu-toggle"><?php _e( 'Primary Menu', 'siidist' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-			</nav><!-- #site-navigation -->
-		
-			<?php get_sidebar(); ?>
-		</header><!-- #masthead -->
+			</div>
+			<?php //get_sidebar(); ?>
+		</div>
 
-
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+		<div class="content-area">
+			<div class="site-main">
