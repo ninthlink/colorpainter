a7a8ad00bdec1f3ee4cac11d173f2a7c17c03c97
alex chousmith <alex@ninthlink.com>
2014-07-03 06:15:01 +0000

    cleanup some visual composer style & js includes

--- dist-app/wptheme/siidist/functions.php
+++ dist-app/wptheme/siidist/functions.php
@@ -93,14 +93,19 @@ add_action( 'widgets_init', 'siidist_widgets_init' );
  */
 function siidist_scripts() {
 	wp_enqueue_style( 'siidist-style', get_stylesheet_uri() );
-
+	
+	wp_deregister_style( 'js_composer_front' );
+	wp_deregister_style( 'ultimate-animate' );
+	wp_deregister_style( 'ultimate-style' );
+	
 	wp_enqueue_script( 'siidist-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
+	/*
 	wp_enqueue_script( 'siidist-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+	*/
 }
 add_action( 'wp_enqueue_scripts', 'siidist_scripts' );
 
