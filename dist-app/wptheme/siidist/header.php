324eac8a6781f3774de38226ec2ea0d7f3c09d92
alex chousmith <alex@ninthlink.com>
2014-07-03 23:28:25 +0000

    more cleanup + flushing out left sidebar

--- dist-app/wptheme/siidist/header.php
+++ dist-app/wptheme/siidist/header.php
@@ -20,12 +20,22 @@
 <div class="rap">
 	<div class="site-content">
 		<div class="site-header">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="logo"><img src="<?php bloginfo('template_url'); ?>/images/logo-sii.png" alt="<?php esc_attr_e( get_bloginfo( 'name' ) ); ?>" /></a>
 			<div class="main-navigation" role="navigation">
 				<button class="menu-toggle"><?php _e( 'Primary Menu', 'siidist' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 			</div>
 			<?php //get_sidebar(); ?>
+			<div class="left-end">
+				<div class="hello">Hello, Distributor</div>
+				<h6><a href="#">LOGOUT</a></h6>
+				<ul class="soc">
+					<li class="fb"><a href="#">Facebook</a></li>
+					<li class="tw"><a href="#">Twitter</a></li>
+					<li class="yt"><a href="#">YouTube</a></li>
+				</ul>
+				<div class="shg">Seiko Holdings Group</div>
+			</div>
 		</div>
 
 		<div class="content-area">
