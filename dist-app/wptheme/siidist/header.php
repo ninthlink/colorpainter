0ba9b63d8d8342b6127a02ce89e054fe6f02d5d9
alex chousmith <alex@ninthlink.com>
2014-07-04 05:54:28 +0000

    homepage downloadpdf btn styling + Why Seiko

--- dist-app/wptheme/siidist/header.php
+++ dist-app/wptheme/siidist/header.php
@@ -11,7 +11,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title><?php wp_title( '' ); ?></title>
 
 <?php wp_head(); ?>
 </head>
