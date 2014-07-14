1588da7fe2180ff8eb06ba01e02f49eadbbe026b
alex chousmith <alex@ninthlink.com>
2014-07-14 21:54:37 +0000

    oops change set WP_HOME + WP_SITEURL + change default app name

--- dist-app/wptheme/siidist/header.php
+++ dist-app/wptheme/siidist/header.php
@@ -14,7 +14,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="HandheldFriendly" content="True">
 <meta name="MobileOptimized" content="768">
-<meta name="apple-mobile-web-app-title" content="Distr0134">
+<meta name="apple-mobile-web-app-title" content="Distributor">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
 <meta name="apple-touch-fullscreen" content="yes">
