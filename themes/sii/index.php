cd63aed30cc30a28fcfb3dd257aa7949245b0b34
alex chousmith <alex@ninthlink.com>
2014-12-15 13:39:03 -0800

    SIIR-8 #comment move "clean and simple" index.php to page.php and make index.php styled for /blog landing

--- themes/sii/index.php
+++ themes/sii/index.php
@@ -1,16 +1,57 @@
 <?php
 /**
- * Clean and simple
+ * main loop for /blog landing?
  *
  * @package sii
  */
 
 get_header(); ?>
+<div class="vc_row wpb_row vc_row-fluid bloghdr">
+  <div class="vc_col-sm-12 wpb_column vc_column_container">
+    <div class="wpb_wrapper">
+      <div class="ult-spacer" style="height:60px;clear:both;display:block;"></div>
+      <div class="wpb_text_column wpb_content_element ">
+        <div class="wpb_wrapper">
+          <h1>SII Blog</h1>
+        </div> 
+      </div>
+      <div class="ult-spacer" style="height:25px;clear:both;display:block;"></div>
+    </div> 
+  </div> 
+</div>
+<div class="vc_row wpb_row vc_row-fluid vc_custom_1418678698057 blogpostscontainer">
+	<div class="vc_col-sm-12 wpb_column vc_column_container">
+		<div class="wpb_wrapper">
+			<div class="vc_row wpb_row vc_inner vc_row-fluid">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<div class="entry-content">
-						<?php the_content(); ?>
-					</div>
-				</div>
+        <div class="vc_col-sm-4 wpb_column vc_column_container blogpost">
+          <div class="wpb_wrapper">
+            <a href="<?php the_permalink(); ?>">
+              <div class="wpb_single_image wpb_content_element vc_align_left">
+                <div class="wpb_wrapper">
+                  <?php if ( has_post_thumbnail() ) {
+                    the_post_thumbnail( 'blogthird' );
+                  } else {
+                    echo '<img src="'. get_bloginfo('template_url') .'/images/blog-default.jpg" alt="Seiko Instruments" />';
+                  } ?>
+                </div> 
+              </div> 
+              <div class="wpb_text_column wpb_content_element ">
+                <div class="wpb_wrapper">
+                  <h3><?php the_title(); ?></h3>
+                </div> 
+              </div> 
+              <div class="wpb_text_column wpb_content_element ">
+                <div class="wpb_wrapper">
+                  <h6 class="byline"><?php the_time('F j, Y'); ?></h6>
+                </div> 
+              </div>
+            </a>
+          </div>
+        </div> 
 			<?php endwhile; // end of the loop. ?>
+      </div>
+		</div> 
+	</div> 
+</div>
 <?php get_footer(); ?>
---
+++ themes/sii/page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Clean and simple
+ *
+ * @package sii
+ */
+
+get_header(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div>
+				</div>
+			<?php endwhile; // end of the loop. ?>
+<?php get_footer(); ?>
