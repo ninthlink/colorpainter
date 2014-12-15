1313214def36f8ff668e736fab4c5e2ee1393b89
alex chousmith <alex@ninthlink.com>
2014-12-15 12:19:10 -0800

    SIIR-8 #comment dont add the quicklinks jump points to the right side for Blog posts

--- themes/sii/js/sii.js
+++ themes/sii/js/sii.js
@@ -67,66 +67,68 @@ jQuery(function($) {
 		$(this).add($(this).children('.imapper-content')).height(th);
 	});
 	
-	/* section quicklinks? */
-	var sitemain = $('.site-main');
-	//sitemain.find('.wpb_row:first').addClass('section').prepend('<a id="top"></a>');
-	var wpul = $('<ul />').addClass('w');
-	wpul.appendTo(sitemain);
-	//wpul.prepend('<li class="active"><a href="#top" class="vc_btn vc_btn_blue vc_btn_xs vc_btn_round">Overview</a></li>');
-	var sections = [];
-	// first
-	$('.site-main .wpb_row:first').each(function() {
-		if ( $(this).hasClass('section') == false ) {
-			$(this).addClass('section hh').prepend('<h2 class="hh">Overview</h2>');
-		}
-	});
-	$('.section').each(function(i) {
-		var thismarker = false;
-		var thisid = '';
-		var thissection = $(this);
-		$(this).find('h2:first').each(function() {
-			var ht = $(this).text();
-			if ( i > 0 ) {
-				thisid = ht;
-				var lastspace = ht.lastIndexOf(' ');
-				if ( lastspace > 0 ) {
-					thisid = ht.substr(lastspace + 1);
-				}
-				thisid = thisid.replace(/\s+/g, '-').toLowerCase();
-				thissection.attr('id', thisid);
-			}
-			// also floating nav waypoints
-			thismarker = $('<li />');
-			if ( i == 0 ) {
-				thismarker.addClass('active');
-			}
-			thismarker.append('<a href="#'+ thisid +'" class="vc_btn vc_btn_blue vc_btn_xs vc_btn_round">'+ ht +'</a>');
-			thismarker.appendTo(wpul);
-		});
-		if ( thismarker != false ) {
-			$(this).waypoint(function(direction) {
-				if ( direction === 'down' ) {
-					thismarker.addClass('active').siblings().removeClass('active');
-				} else {
-					thismarker.removeClass('active').prev().addClass('active');
-				}
-			}, {
-				offset: 70
-			});
-			sections.push(thissection);
-			thismarker.children('a').click(function(event) {
-				event.preventDefault();
-				var scrollto = 0;
-				if ( i > 0 ) {
-					scrollto = thissection.offset();
-					scrollto = Math.floor(scrollto.top);
-				}
-				//alert('hello? '+ cht + ' ' + bht + ' > ' + scrollto);
-				var thisa = $(this);
-				$('html,body').animate({'scrollTop': scrollto}, 300, function() {
-					window.location.hash = thisid;
-				});
-			});
-		}
-	});
+  if ( body.hasClass('single-post') == false ) {
+    // section quicklinks
+    var sitemain = $('.site-main');
+    //sitemain.find('.wpb_row:first').addClass('section').prepend('<a id="top"></a>');
+    var wpul = $('<ul />').addClass('w');
+    wpul.appendTo(sitemain);
+    //wpul.prepend('<li class="active"><a href="#top" class="vc_btn vc_btn_blue vc_btn_xs vc_btn_round">Overview</a></li>');
+    var sections = [];
+    // first
+    $('.site-main .wpb_row:first').each(function() {
+      if ( $(this).hasClass('section') == false ) {
+        $(this).addClass('section hh').prepend('<h2 class="hh">Overview</h2>');
+      }
+    });
+    $('.section').each(function(i) {
+      var thismarker = false;
+      var thisid = '';
+      var thissection = $(this);
+      $(this).find('h2:first').each(function() {
+        var ht = $(this).text();
+        if ( i > 0 ) {
+          thisid = ht;
+          var lastspace = ht.lastIndexOf(' ');
+          if ( lastspace > 0 ) {
+            thisid = ht.substr(lastspace + 1);
+          }
+          thisid = thisid.replace(/\s+/g, '-').toLowerCase();
+          thissection.attr('id', thisid);
+        }
+        // also floating nav waypoints
+        thismarker = $('<li />');
+        if ( i == 0 ) {
+          thismarker.addClass('active');
+        }
+        thismarker.append('<a href="#'+ thisid +'" class="vc_btn vc_btn_blue vc_btn_xs vc_btn_round">'+ ht +'</a>');
+        thismarker.appendTo(wpul);
+      });
+      if ( thismarker != false ) {
+        $(this).waypoint(function(direction) {
+          if ( direction === 'down' ) {
+            thismarker.addClass('active').siblings().removeClass('active');
+          } else {
+            thismarker.removeClass('active').prev().addClass('active');
+          }
+        }, {
+          offset: 70
+        });
+        sections.push(thissection);
+        thismarker.children('a').click(function(event) {
+          event.preventDefault();
+          var scrollto = 0;
+          if ( i > 0 ) {
+            scrollto = thissection.offset();
+            scrollto = Math.floor(scrollto.top);
+          }
+          //alert('hello? '+ cht + ' ' + bht + ' > ' + scrollto);
+          var thisa = $(this);
+          $('html,body').animate({'scrollTop': scrollto}, 300, function() {
+            window.location.hash = thisid;
+          });
+        });
+      }
+    });
+  }
 });
